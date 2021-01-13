833a5ef2cc5f098bbb20e96c5fd37f42f0fb732a
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-01-13 20:43:16 +0200

    Add files via upload

--- Syntax, Functions and Statements/cookingByNumbers.js
+++ Syntax, Functions and Statements/cookingByNumbers.js
@@ -1,34 +1,25 @@
 function solve(...params){
     let num = Number(params.shift())
-    let result = 0
 
-    let commands = params.shift();
-    while (params > 0){
+    for (let commands of params){
         if (commands === 'chop'){
-            result = num / 2;
-            console.log(result)
+            num = num / 2;
+            console.log(num)
         } else if (commands === 'dice'){
-            let iteractions = 0
-            for (let i = result; i > 0; i--){
-                if (i % 3 === 0){
-                result -= i
-                iteractions += 1
-                }
-            }
-            result = iteractions
-            console.log(result)
+            num = Math.sqrt(num)
+            console.log(num)
         } else if (commands === 'spice'){
-            result += 1
-            console.log(result)
+            num += 1
+            console.log(num)
         } else if (commands === 'bake'){
-            result *= 3
-            console.log(result)
+            num *= 3
+            console.log(num)
         } else if (commands === 'fillet'){
-            result -= result * 0.2
-            console.log(result)
+            num -= num * 0.2
+            console.log(num)
         }
-        commands = params.shift();
     }
 }
 
-solve ('32', 'chop', 'chop', 'chop', 'chop', 'chop')
+//solve ('32', 'chop', 'chop', 'chop', 'chop', 'chop')
+solve('9', 'dice', 'spice', 'chop', 'bake', 'fillet')
