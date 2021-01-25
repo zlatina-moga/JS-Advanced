2cbf1ceac8b93f2e57910d382ca598aca5f8c98d
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-01-25 08:22:19 +0200

    Add files via upload

---
+++ Objects and Composition/Lab/cityRecord.js
@@ -0,0 +1,12 @@
+function solve(name, population, treasury){
+    const town = {
+        name: name,
+        population: population,
+        treasury: treasury
+    }
+    return town;
+}
+
+solve('Tortuga',
+7000,
+15000)
