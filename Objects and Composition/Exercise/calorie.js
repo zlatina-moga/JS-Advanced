3a3603f8966da59dd7cf648a341b9acda0c2404a
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-01-26 08:48:06 +0200

    Add files via upload

---
+++ Objects and Composition/Exercise/calorie.js
@@ -0,0 +1,13 @@
+function solve(arr){
+    let result = {};
+    for (let i = 0; i < arr.length; i++){
+        if (i % 2 == 0){
+            result[arr[i]] = Number(arr[i + 1])
+        }
+    }
+    console.log(result)
+}
+
+
+//solve(['Yoghurt', '48', 'Rise', '138', 'Apple', '52'])
+solve(['Potato', '93', 'Skyr', '63', 'Cucumber', '18', 'Milk', '42'])
