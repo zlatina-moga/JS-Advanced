a3525957f607934386ee35e9d59a93a63b2e8b60
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-01-29 12:03:40 +0200

    Add files via upload

---
+++ DOM/Exercise/01. Subtraction/subtract.js
@@ -0,0 +1,6 @@
+function subtract() {
+    const firstNum = document.getElementById('firstNumber').value;
+    const secondNum = document.getElementById('secondNumber').value;
+    const result = Number(firstNum) - Number(secondNum)
+    document.getElementById('result').textContent = result;
+}
