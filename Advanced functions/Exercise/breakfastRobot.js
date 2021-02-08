3802a8a7b2ec2c23dd632b0c742cd3aa64b38de8
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-02-08 15:17:33 +0200

    Add files via upload

---
+++ Advanced functions/Exercise/breakfastRobot.js
@@ -0,0 +1,45 @@
+function solution() {
+    const recipes = {
+        apple: {carbohydrate: 1, flavour: 2},
+        lemonade: {carbohydrate: 10, flavour: 20},
+        burger: {carbohydrate: 5, fat: 7, flavour: 3},
+        eggs: {protein: 5, fat: 1, flavour: 1},
+        turkey: {protein: 10, fat: 10, carbohydrate: 10, flavour: 10}
+    };
+
+    const stocks = {
+        protein: 0,
+        carbohydrate: 0,
+        fat: 0,
+        flavour: 0
+    }
+
+    const commands = {
+        restock: (microElement, quantity) => {
+            stocks[microElement] += quantity
+            return 'Success';
+        },
+        prepare: (product, quantity) => {
+            let recipe = Object.entries(recipes[product]);
+            for (let [item, countNeeded] of recipe){
+                if (stocks[item] < countNeeded * quantity){
+                    return `Error: not enough ${item} in stock`
+                }
+            }
+            recipe.forEach(([item, countNeeded]) => {
+                stocks[item] -= countNeeded * quantity
+            });
+            return 'Success';
+        },
+        report : () => Object.entries(stocks).map(([microElement, count]) => `${microElement}=${count}`).join(' ')
+    }
+
+    return (input) => {
+        let [command, item, count] = input.split(' ');
+        return commands[command](item, +count)
+    }
+}
+
+let manager = solution (); 
+console.log (manager ("restock flavour 50")); // Success 
+console.log (manager ("prepare lemonade 4")); // Error: not enough carbohydrate in stock 
