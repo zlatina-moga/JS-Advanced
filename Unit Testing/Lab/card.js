1b3852eb6a201cfc9f16345f9661d09f2533a63c
Zlatina Moga <72757516+zlatina-moga@users.noreply.github.com>
2021-02-09 14:23:04 +0200

    Add files via upload

---
+++ Unit Testing/Lab/card.js
@@ -0,0 +1,25 @@
+function createCard(face, suit){
+    const validFace = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    const suitToString = {
+        'S': '\u2660',
+        'H': '\u2665',
+        'D': '\u2666',
+        'C': '\u2663'
+    }
+
+    if (validFace.includes(face) == false){
+        throw new Error('Invalid face')
+    } else if (Object.keys(suitToString).includes(suit) == false){
+        throw new Error('Invalid suit')
+    }
+
+    return {
+        face,
+        suit,
+        toString(){
+            return `${face}${suitToString[suit]}`
+        }
+    }
+}
+
+createCard('J', 'D')
